0cca656cb7127e74f8ab0db6c3aba96d72820ae6
Gabr13dev <gabriel.almeida.p@outlook.com>
2021-02-24 07:31:53 -0300

    Commit inicial

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "90%" }} />
+
+        <span className="current-experience" style={{ left: "90%" }}>
+          500xp
+        </span>
+      </div>
+      <span>600 px</span>
+    </header>
+  );
+}
